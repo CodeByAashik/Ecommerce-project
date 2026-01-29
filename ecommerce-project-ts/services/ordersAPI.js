70b1226347f95aefd458c76e030b3e2ef8fdf5c3
CodeByAashik <thakuraashik98121@gmail.com>
2026-01-29 23:22:06 +0545

    review code

--- ecommerce-project-ts/services/ordersAPI.js
+++ ecommerce-project-ts/services/ordersAPI.js
@@ -3,4 +3,4 @@ import axios from 'axios';
 export const loadCart = async (orderId, setOrder) => {
     const response = await axios.get(`/api/orders/${orderId}?expand=products`);
     setOrder(response.data);
-};  
+};    
